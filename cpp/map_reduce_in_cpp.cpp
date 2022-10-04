ccfc0fc41caef21ed8ff9abb7603c79db3b9999a
Yanrui Hu <yanruinku@qq.com>
2022-10-05 00:10:12 +0800

    Update accumulate.

--- cpp/map_reduce_in_cpp.cpp
+++ cpp/map_reduce_in_cpp.cpp
@@ -11,8 +11,29 @@
 #include <list>
 using namespace std;
 
-int n[] = {1, 2, 3, 4};
-std::cout << std::accumulate(n, n + 4, 0) << std::endl;
+// C++ 中的 accumulate 第一个和第二个参数是输入的范围，第三个参数是 initial
+// 使用 + 运算符进行累加操作
+
+// 函数原型 1，使用加法运算符
+// template <class InputIterator, class T>
+// T accumulate (InputIterator first, InputIterator last, T init);
+
+// 函数原型 2, 自定义运算
+// template <class InputIterator, class T, class BinaryOperation>
+// T accumulate (InputIterator first, InputIterator last, T init,
+// BinaryOperation binary_op);
+/* EXAMPLE */
+
+// int n[] = {1, 2, 3, 4};
+// std::cout << std::accumulate(n, n + 4, 0) << std::endl;
+// 不带运算函数参数的std::accumulate对应的是Python 中的sum函数
+
+// Python 中的 reduce 第一个参数是 function, 然后是一个 iterable, 最后有一个可选的 initial
+// def add(x, y):
+//     return x+y
+
+// from functools import reduce
+// reduce(add, [1,2,3,4])
 
 template <typename Func, typename Container>
 auto reduce(Func f, Container& container) -> decltype(Container::value_type()) {
